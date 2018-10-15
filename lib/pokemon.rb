3dd33b95eaccaf010f3123fe69476352faffff97
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-15 17:08:10 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,5 +14,6 @@ class Pokemon
     pokemon = ALL.detect { |p| p.name == name && p.type == type }
     sql = db.prepare("INSERT INTO pokemon (id, name, type) values (?, ?, ?)")
     sql.execute("test")
+    binding.pry
   end
 end
